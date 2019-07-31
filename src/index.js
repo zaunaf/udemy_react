15e377f65f884deca7a879b48f86a72b89b01630
Abah Z <zaunaf@yahoo.com>
2019-07-31 13:47:17 +0700

    Step 03 - Membuat App component

--- src/index.js
+++ src/index.js
@@ -1,6 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {createStore} from 'redux';
+import createHistory from 'history/createBrowserHistory';
+import {Provider} from 'react-redux';
+import {ConnectedRouter} from 'react-router-redux';
+import {Route} from 'react-router';
+import App from "./components/App";
+
+const store = createStore(
+    state => state
+);
+
+const history = createHistory();
 
 ReactDOM.render((
-    <div>Bismillah.. Assalaamu alaikum world</div>
+    <Provider store={store}>
+        <ConnectedRouter history={history}>
+            <Route path="/" component={App}/>
+        </ConnectedRouter>
+    </Provider>
+    
 ), document.getElementById('root'));
